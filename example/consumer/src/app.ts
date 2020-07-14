bfcd965b78d7a7656a98c005da157beb21a56f7f
Andreas Bauer <dev@andreasbauer.org>
2020-07-14 21:10:47 +0200

    Refactor consumer

--- example/consumer/src/app.ts
+++ example/consumer/src/app.ts
@@ -1,48 +1,62 @@
-import * as AMQP from 'amqplib'
+import * as AMQP from "amqplib";
 
-const AMQP_URL = process.env.AMQP_URL
-const exchange = process.env.AMQP_EXCHANGE
-const key = process.env.AMQP_TOPIC
+const AMQP_URL = process.env.AMQP_URL;
+const EXCHANGE = process.env.AMQP_EXCHANGE;
+const MAX_RETRIES = 30;
 
+const main = async () => {
+  console.log("[SETUP] Starting Producer Microservice Instance...");
+  console.log(`[SETUP] URL of AMQP Broker: ${AMQP_URL}`);
+
+  try {
+    const con = await connect(AMQP_URL);
+    const channel = await con.createChannel();
+    channel.assertExchange(EXCHANGE, "topic", { durable: false });
+    const q = await channel.assertQueue("consumer_queue", { exclusive: false });
+    channel.bindQueue(q.queue, EXCHANGE, "#");
+    channel.consume(q.queue, handleMsg);
+  } catch (e) {
+    console.error("[FAIL] Unable to connect to AMQP broker! \n%s", e);
+    process.exit(1);
+  }
+
+  console.log("[READY] Consumer is ready to retrieve messages.");
+};
+
+const handleMsg = async (msg: AMQP.ConsumeMessage) => {
+  console.log(
+    "[Consumer] %s:'%s'",
+    msg.fields.routingKey,
+    msg.content.toString()
+  );
+
+  await sleep(100); // simulate msg processing time
+};
+
+const connect = async (amqpUrl: string): Promise<AMQP.Connection> => {
+  let retry = 0;
+  while (retry < MAX_RETRIES) {
+    try {
+      console.log(
+        `[SETUP] Try to connect to AMQP broker. [%i retries remaining]`,
+        MAX_RETRIES - retry
+      );
+
+      const connection = await AMQP.connect(amqpUrl);
+      console.log("[SUCCESS] Connected to AMQP Broker.");
+
+      return Promise.resolve(connection);
+    } catch (e) {
+      retry++;
+      await sleep(2000);
+    }
+  }
+
+  return Promise.reject();
+};
 
 const sleep = (ms: number): Promise<void> => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-const connect = async (amqpUrl: string): Promise<AMQP.Connection>  => {
-    let retry = 0
-    while(retry < 30) {
-        try {
-            console.log("Attempting to connect to AMQP Broker.")
-            const connection = await AMQP.connect(amqpUrl)
-            console.log("Connected to AMQP Broker.")
-            return Promise.resolve(connection)
-        } catch (e) {
-            retry ++
-            await sleep(2000)
-        }
-    }
-    console.log("Could not connect to AMQP Broker!")
-    return Promise.reject()
-}
-
-
-console.log("Starting Producer Microservice Instance...")
-console.log(`URL of AMQP Broker: ${AMQP_URL}`)
-connect(AMQP_URL).then(async (connection: AMQP.Connection) => {
-    const channel = await connection.createChannel()
-    channel.assertExchange(exchange, 'topic', {
-        durable: false
-      });
-
-    const q = await channel.assertQueue('consumer_queue', {
-        exclusive: false
-    })
-    console.log("Consumer waiting for messages to arrive...");
-
-    channel.bindQueue(q.queue, exchange, "#");
-    channel.consume(q.queue, async (msg: AMQP.ConsumeMessage) => {
-        console.log("[Consumer] %s:'%s'", msg.fields.routingKey, msg.content.toString());
-        await sleep(100) // simulate small msg processing time
-    })
-})
+main();
